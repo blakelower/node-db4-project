b5f993f3d468d8fea48c1a0b2bbf537dd7abc166
jordan <jordan@jordans-MacBook-Air.local>
2020-01-23 20:37:36 +0700

    added migrations and tables

--- data/migrations/20200123202515_recipetables.js
+++ data/migrations/20200123202515_recipetables.js
@@ -1,8 +1,52 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+    .createTable("recipes", tbl => {
+      tbl.increments();
+      tbl.string("name").notNullable();
+    })
+    .createTable("ingredients", tbl => {
+      tbl.increments();
+      tbl.string("name").notNullable();
+      tbl.float("quantity").notNullable();
+    })
+    .createTable("recipe_ingredients", tbl => {
+      tbl.primary(["recipe_id", "ingredient_id"]);
+
+      // foriegn key
+      tbl.integer("recipe_id")
+        .unsigned()
+        .references("id")
+        .inTable("recipes")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+
+      // foriegn key
+      tbl.integer("ingredient_id")
+        .unsigned()
+        .references("id")
+        .inTable("ingredients")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("instructions", tbl => {
+      tbl.increments();
+      tbl.text("instructions").notNullable();
+
+      // foriegn key
+      tbl.integer("recipe_id")
+        .unsigned()
+        .references("id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .dropTableIfExists("recipes")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipe_ingredients")
+    .dropTableIfExists("instructions");
 };
